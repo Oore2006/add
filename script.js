a8511f8d571f24bd251962069b9370b7a90db646
Oore2006 <goodnessoluseyi@gmail.com>
2023-08-15 11:04:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const alter= document.getElementById("hello");
+ 
+
+alter.onclick= function() {
+    alert("ready to try again");
+};
+const red=23;
+let us=25;
+document.getElementById("yes").externalHTML = red + us ;
+
+
